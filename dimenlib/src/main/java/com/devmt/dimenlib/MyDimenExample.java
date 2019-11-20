60347e8a0afebbba2899e47fd41be8369e1d9336
devmt <chudat97@gmail.com>
2019-11-21 03:38:37 +0700

    update

---
+++ dimenlib/src/main/java/com/devmt/dimenlib/MyDimenExample.java
@@ -0,0 +1,10 @@
+package com.devmt.dimenlib;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyDimenExample extends View {
+    public MyDimenExample(Context context) {
+        super(context);
+    }
+}
